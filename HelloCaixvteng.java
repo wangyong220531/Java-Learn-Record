0f7421c20db97ee00d452fa507a9f6a70d3af2f2
王勇 <wy19945372694@163.com>
2023-05-17 14:37:28 +0800

    update

---
+++ HelloCaixvteng.java
@@ -0,0 +1,15 @@
+
+public class HelloCaixvteng {
+    public static void main(String[] args) {
+        System.out.println("Hello! Caixvteng");
+        System.out.print("Hello! Bufanghao");
+    }
+}
+
+class Animal {
+
+}
+
+class People {
+
+}
--- HelloWorld.java
+++
@@ -1,5 +0,0 @@
-class HelloCaixvteng {
-    public static void main(String[] args){
-       System.out.println("Hello! Caixvteng");
-    }
-}
